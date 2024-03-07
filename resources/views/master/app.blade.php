3b51ee4009024a7dfba0f206dd44f818d94be8ed
ah-badrr <ahmadibadurrahman03@gmail.com>
2024-03-07 12:06:32 +0700

    Tambahkan screenshot

--- resources/views/master/app.blade.php
+++ resources/views/master/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
     <!-- site metas -->
-    <title>Tapet V.2.0</title>
+    <title>UNIBOOKSTORE</title>
     <meta name="keywords" content="">
     <meta name="description" content="">
     <meta name="author" content="">
