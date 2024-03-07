8f48199e47be454d81b63e6ddc777787ffd3839a
ah-badrr <ahmadibadurrahman03@gmail.com>
2024-03-07 08:11:34 +0700

    Buat konfig authentication

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -11,6 +11,23 @@ use Illuminate\Validation\ValidationException;
 
 class LoginRequest extends FormRequest
 {
+    protected $id_type;
+
+    protected function prepareForValidation(): void
+    {
+
+        // if kalu nginputin email
+        if (filter_var($this->input('userid'), FILTER_VALIDATE_EMAIL)) {
+            $this->id_type = 'email';
+        } else {
+            // else kalu nginputin username
+            $this->id_type = 'username';
+        }
+
+        $this->merge([
+            $this->id_type => $this->input('userid'),
+        ]);
+    }
     /**
      * Determine if the user is authorized to make this request.
      */
@@ -27,7 +44,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ['required', 'string', 'email'],
+            'userid' => ['required', 'string'],
             'password' => ['required', 'string'],
         ];
     }
@@ -41,11 +58,11 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        if (!Auth::attempt($this->only($this->id_type, 'password'), $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
+                'userid' => trans('auth.failed'),
             ]);
         }
 
@@ -59,7 +76,7 @@ class LoginRequest extends FormRequest
      */
     public function ensureIsNotRateLimited(): void
     {
-        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+        if (!RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
             return;
         }
 
@@ -80,6 +97,6 @@ class LoginRequest extends FormRequest
      */
     public function throttleKey(): string
     {
-        return Str::transliterate(Str::lower($this->string('email')).'|'.$this->ip());
+        return Str::transliterate(Str::lower($this->string('email')) . '|' . $this->ip());
     }
 }
